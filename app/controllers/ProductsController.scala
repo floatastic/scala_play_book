8463a9f2ef44f42516fc026e89f14a9026f59a69
Michal Kreft <michal.krefts@johnlewis.co.uk>
2017-07-04 23:18:29 +0200

    add products list

---
+++ app/controllers/ProductsController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import javax.inject._
+
+import play.api._
+import play.api.mvc._
+import models.Product
+import play.api.i18n.{I18nSupport, MessagesApi}
+
+@Singleton
+class ProductsController @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  def list = Action { implicit request =>
+    val products = Product.findAll
+    Ok(views.html.products.list(products))
+  }
+}
